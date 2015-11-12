99a1c91b2d913d3fbf7a57556df99fe22afe60ed
Max Wenzl <max@Max.Home>
2015-11-12 11:24:39 +0100

    some changes in the aes

--- CTF/Service/aes/aesopenssl.c
+++ CTF/Service/aes/aesopenssl.c
@@ -1,6 +1,29 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+
+char *randstring(size_t length) 
+{
+
+    static char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";        
+    char *randomString = NULL;
+    int n = 0;
+    if (length) {
+        randomString = malloc(sizeof(char) * (length +1));
+
+        if (randomString) {            
+            for (n = 0;n < length;n++) {            
+                int key = rand() % (int)(sizeof(charset) -1);
+                randomString[n] = charset[key];
+            }
+
+            randomString[length] = '\0';
+        }
+    }
+
+    return randomString;
+}
+
 int main(int argc, char* argv[])
 {
 	if (argc == 3)
@@ -12,13 +35,32 @@ int main(int argc, char* argv[])
 			popen(tmp, "w");
 			return 0;		
 		}
-		else if(argv[1], "decrypt")
+		else if(strcmp(argv[1], "decrypt")==0)
 		{
+			char* randName = randstring(15);
+			FILE* f = NULL;
 			char* tmp = malloc(sizeof(char)*2000);
 			char* remove = malloc(sizeof(char)*200);
-			sprintf(tmp, "openssl aes-256-cbc -d -in %s.enc -out %s.out -pass pass:Â§acf578?#*+-463-{{}av@wer637,,..", argv[2], argv[2]);
-			FILE* f = popen(tmp, "w");
+			sprintf(tmp, "openssl aes-256-cbc -d -in %s.enc -out %s -pass pass:Â§acf578?#*+-463-{{}av@wer637,,..", argv[2], randName);
+			printf("%s\n", randName);
+			f = popen(tmp, "w");
 			pclose(f);
+			FILE* output=fopen(randName, "r");
+			char* encryptedData[1500];
+			int i = 0;
+			encryptedData[0] = malloc(sizeof(char)*500);
+			char line[1024];
+			while(fgets(line, 1024, output)!=NULL)
+			{
+				sprintf(encryptedData[i], "%s", line); 
+				i++;
+				encryptedData[i] = malloc(sizeof(char)*500);
+			}
+			int p = 0;
+			for(p = 0; p < 15; p++)
+				printf("---------------------------------------->%s\n", encryptedData[p]);
+			
+			
 			/*
 			FILE *test = fopen("Bayrisch.csv.out");
 			char line[1024];
@@ -28,7 +70,7 @@ int main(int argc, char* argv[])
 			 * 
 			 *
 			 */
-			sprintf(remove, "rm %s.out", argv[2]);
+			sprintf(remove, "rm %s", randName);
 			popen(remove, "w");
 			return 0;
 		}
