116a5eee3bc457b8823f35014c6f4de5b04a6df5
Dominik Schlecht <dominik.schlecht@hotmail.de>
2015-11-15 19:40:48 +0100

    Implemented format string exploit

--- CTF/Service/aesopenssl.c
+++ CTF/Service/aesopenssl.c
@@ -1,20 +1,20 @@
-#include <Python.h>
+//#include <Python.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
 
-char *randstring(size_t length) 
+char *randstring(size_t length)
 {
 
-    static char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";        
+    static char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
     char *randomString = NULL;
     int n = 0;
     if (length) {
         randomString = malloc(sizeof(char) * (length +1));
 
-        if (randomString) {            
-            for (n = 0;n < length;n++) {            
+        if (randomString) {
+            for (n = 0;n < length;n++) {
                 int key = rand() % (int)(sizeof(charset) -1);
                 randomString[n] = charset[key];
             }
@@ -50,12 +50,10 @@ const char* translator(const char* word)
 			char* tmp = malloc(sizeof(char)*2000);
 			sprintf(tmp, "openssl aes-256-cbc -in %s -out %s.enc -pass pass:Â§acf578?#*+-463-{{}av@wer637,,..", argv[2], argv[2]);
 			popen(tmp, "w");
-			return 0;		
+			return 0;
 		}
 		else if(strcmp(argv[1], "decrypt")==0)
 		{*/
-
-		
 	char* randName = randstring(15);
 	FILE* f = NULL;
 	char* tmp = malloc(sizeof(char)*2000);
@@ -64,12 +62,18 @@ const char* translator(const char* word)
 	f = popen(tmp, "w");
 	pclose(f);
 	FILE* output=fopen(randName, "r");
-	char* encryptedData[1500];
+
 	int i = 0;
 	int p = 1;
+  int j = 0;
+  int arLen;
+  int len;
 	char* result = malloc(128);
-	encryptedData[0] = malloc(sizeof(char)*500);
 	char line[1024];
+  char* test = word;
+  char* encryptedData[1500];
+  encryptedData[0] = malloc(sizeof(char)*500);
+
 	while(fgets(line, 1024, output)!=NULL)
 	{
 		char* tmp = strdup(line);
@@ -78,19 +82,65 @@ const char* translator(const char* word)
 			strcpy(tmp, line);
 			strcpy(result, getfield(tmp,2));
 		}
-		sprintf(encryptedData[i], "%s", line); 
+		sprintf(encryptedData[i], "%s", line);
 		i++;
 		encryptedData[i] = malloc(sizeof(char)*500);
 		free(tmp);
 	}
+  arLen = i;
+  char *sep = "4242";
+  len = strlen(sep);
+  for (i = 0; i < len; i++){
+    __asm__ volatile (  "push  %%rax\n"
+                      //"popq   %%rax\n"
+
+                    :           /* OUTPUT values         */
+                    : "a"(sep[i])           /* INPUT  values         */
+                    :    /*No need for the clobber list, since the compiler knows
+                        which registers have been modified            */
+                );
+  }
+  for (j = 0; j < arLen; j++){
+    char *a = encryptedData[j];
+    len = strlen(a);
+    for (i = 0; i < len; i++){
+      __asm__ volatile (  "push  %%rax\n"
+                        //"popq   %%rax\n"
+
+                      :           /* OUTPUT values         */
+                      : "a"(a[i])           /* INPUT  values         */
+                      :    /*No need for the clobber list, since the compiler knows
+                          which registers have been modified            */
+                  );
+    }
+    len = strlen(sep);
+    for (i = 0; i < len; i++){
+      __asm__ volatile (  "push  %%rax\n"
+                        //"popq   %%rax\n"
+
+                      :           /* OUTPUT values         */
+                      : "a"(sep[i])           /* INPUT  values         */
+                      :    /*No need for the clobber list, since the compiler knows
+                          which registers have been modified            */
+                  );
+    }
+  }
+  printf(test);
+
 	/* give result to the next function */
 	sprintf(remove, "rm %s", randName);
 	popen(remove, "w");
 	return result;
 }
 
+int main(int argc, char *argv[])
+{
+  char* test = argv[1];
+  char* a = translator(test);
+  printf(a);
+}
 
-/*Python Stuff */
+/*Python Stuff
 static PyObject* translation(PyObject* self, PyObject* args)
 {
 	const char* command;
@@ -98,11 +148,11 @@ static PyObject* translation(PyObject* self, PyObject* args)
 
 	if(!PyArg_ParseTuple(args, "s", &command))
 		return NULL;
-	
+
 	return Py_BuildValue("s", translator(command));
 }
 
-static PyMethodDef aesopenssl_methods[] = 
+static PyMethodDef aesopenssl_methods[] =
 {
 	{"translation", translation, METH_VARARGS},
 	{NULL, NULL}
@@ -113,4 +163,3 @@ void initaesopenssl()
 	(void) Py_InitModule("aesopenssl", aesopenssl_methods);
 }
 /*Python Stuff */
-
