fe53e0472e1e6defde884c5bc42a2e8cee872e74
Dominik Schlecht <dominik.schlecht@hotmail.de>
2016-01-13 09:57:14 +0100

    Remved iCTF and added description to index

--- WebsidePrototype/run.py
+++ WebsidePrototype/run.py
@@ -6,10 +6,6 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route("/ictf/")
-def ictf():
-    return render_template('ictf.html')
-
 @app.route("/netzwerk/")
 def netzwerk():
     return render_template('netzwerk.html')
