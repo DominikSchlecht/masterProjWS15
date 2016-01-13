a0f0568dd5fa13f4b0148aea7442d39581b790ef
phippsW <phippsW>
2016-01-13 13:25:07 +0100

    ci

---
+++ Vorfuehraufgaben/Philipp - Command Injection/help.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<!-- your webpage info goes here -->
+
+    <title>Security</title>
+	
+	<meta name="author" content="Philipp Weitl" />
+	<meta name="description" content="A Security Website" />
+
+<!-- you should always add your stylesheet (css) in the head tag so that it starts loading before the page html is being displayed -->	
+	<link rel="stylesheet" href="style.css" type="text/css" />
+	
+</head>
+<body>
+
+<!-- webpage content goes here in the body -->
+
+	<div id="page">
+		<div id="logo">
+			<h1><a href="#/home" id="logoLink">Security</a></h1>
+		</div>
+		<div id="nav">
+			<ul>
+				<li><a href="index.php">Home</a></li>
+				<li><a href="about.php">About</a></li>
+				<li><a href="help.php">Help</a></li>
+			</ul>	
+		</div>
+		<div id="content">
+			<h2>Help</h2>
+			<p>
+				The key is at Home!
+			</p>			
+		</div>
+	</div>
+</body>
+</html>
